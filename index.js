51e1110789c182193fc422e5378453f205758624
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-09-20 11:14:50 +0200

    Create variable bankBranchInstance to store singleton instance

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // This one will be a little tricky. So check out this overview first: https://www.youtube.com/watch?v=sJ-c3BA-Ypo
 
 // 1. Create a variable to store the singleton instance of the bank branch. "bankBranchInstance"
-
+let bankBranchInstance = null;
 // 2. Define a class called `BankBranch` for managing branch information.
 
 // 3. In the `BankBranch` class:

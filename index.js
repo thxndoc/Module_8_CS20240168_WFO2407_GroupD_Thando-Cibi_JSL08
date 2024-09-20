f1beebd21d3f64b78a61e3d1b3a0792f094f4e83
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-09-20 11:59:55 +0200

    Create 2 instances of BankBranch

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ class BankBranch {
         return this.branchInfo;
     }
 }
-// 5. In the usage section:
-//    - Create instances of the `BankBranch` class, such as `branchA` and `branchB`, with different branch information.
+// 5. In the usage section: create instances of the `BankBranch` class, such as `branchA` and `branchB`, with different branch information.
+const branchA = new BankBranch({name: "Branch-A", location: "Johannesburg"});
+const branchB = new BankBranch({name: "Branch-B", location: "Pretoria"});
 //    - Use the `getBranchInfo` method to retrieve branch information from the instances.
 //    - Verify that `branchA` and `branchB` are both referring to the same instance by comparing them using `===`.
 

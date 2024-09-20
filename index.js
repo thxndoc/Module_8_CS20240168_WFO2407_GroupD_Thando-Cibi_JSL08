7298a98b6be33b630aacd93c62fb9135ff6da21c
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-09-20 11:46:49 +0200

    Check if instance exists

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ let bankBranchInstance = null;
 
 // 2. Define a class called `BankBranch` for managing branch information.
 class BankBranch {
-// 3. In the `BankBranch` class: create a constructor that takes `branchInfo` as a parameter.
+     //3. In the `BankBranch` class: create a constructor that takes `branchInfo` as a parameter.
     constructor (branchInfo) {
-//    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
-//    - If `bankBranchInstance` is null, create a new instance with the provided `branchInfo` and assign it to `bankBranchInstance`.
-//    - Return the `bankBranchInstance` whether it's newly created or existing.
+       // Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
+        if (bankBranchInstance === null) {
+         // If `bankBranchInstance` is null, create a new instance with the provided `branchInfo` and assign it to `bankBranchInstance`.
+            this.branchInfo = branchInfo;
+            bankBranchInstance = this;
+        }
+        // Return the `bankBranchInstance` whether it's newly created or existing.
+        return bankBranchInstance;
     }
 }
 // 4. Add methods to the `BankBranch` class for managing branch-related information. For example, you can add a method like `getBranchInfo` to retrieve branch details.

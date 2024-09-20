f6a7f86071116de7331b74f4d3d925ca87eae291
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-09-20 11:23:26 +0200

    Create constructor

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
 
 // 1. Create a variable to store the singleton instance of the bank branch. "bankBranchInstance"
 let bankBranchInstance = null;
+
 // 2. Define a class called `BankBranch` for managing branch information.
 class BankBranch {
-// 3. In the `BankBranch` class:
-//    - Create a constructor that takes `branchInfo` as a parameter.
+// 3. In the `BankBranch` class: create a constructor that takes `branchInfo` as a parameter.
+    constructor (branchInfo) {
 //    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
 //    - If `bankBranchInstance` is null, create a new instance with the provided `branchInfo` and assign it to `bankBranchInstance`.
 //    - Return the `bankBranchInstance` whether it's newly created or existing.
+    }
 }
 // 4. Add methods to the `BankBranch` class for managing branch-related information. For example, you can add a method like `getBranchInfo` to retrieve branch details.
 
